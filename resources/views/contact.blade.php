a2ca0db760831cfb432a9c8170cbc36f442538cb
yohanesarmando2007-cloud <yohanesarmando2007@gmail.com>
2026-03-08 16:31:52 +0700

    Praktikum 2 MVC

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,4 @@
+<div>
+    Silangkan menghubungi 0800-0000-0000 untuk informasi lebih lanjut
+    <!-- Simplicity is the essence of happiness. - Cedric Bledsoe -->
+</div>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+<div>
+    selamat datang di tokopeddia {{$nama}}, tempat dimana menjual produk produk secara online <br>
+    anda adalah seorang {{$pekerjaan}}
+    <!-- An unexamined life is not worth living. - Socrates -->
+</div>

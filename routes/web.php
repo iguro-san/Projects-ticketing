c6b14f47226490dfb4f1130dc90e71eb3aefdccb
dhanokk <mnurramadhanirsya@gmail.com>
2026-03-16 16:42:58 +0700

    login route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 Use App\Http\Controllers\HomeController;
 use App\Http\Controllers\DaftarEventController;
+use App\Http\Controllers\LoginController;
 
 // Route::get('/', function () {
 //     return view('welcome');
@@ -12,3 +13,4 @@ Route::get('/', [HomeController::class, 'index']);
 Route::get('/contact', [HomeController::class, 'contact']);
 Route::get('/daftarevent', [DaftarEventController::class, 'index']);
 Route::get('/daftarevent/{id}', [DaftarEventController::class, 'detail']);
+Route::get('/login', [LoginController::class, 'index']);

785d1ae3509ae1845734887074520e6bf7ddc7e4
yohanesarmando2007-cloud <yohanesarmando2007@gmail.com>
2026-06-13 15:52:01 +0700

    Menambahkan sidebar untuk halaman admin dan tampilan responsif

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,4 +1,6 @@
     {{-- Footer --}}
-    <footer class="bg-[#141E46] border-t mt-12 py-4 text-center text-white text-sm">
-        &copy; {{ date('Y') }} EventKu - Event Management System. All rights reserved.
-    </footer>
+@props(['class' => ''])
+
+<footer class="bg-[#141E46] border-t py-4 text-center text-white text-sm {{ $class }}">
+    &copy; {{ date('Y') }} EventKu - Event Management System. All rights reserved.
+</footer>
